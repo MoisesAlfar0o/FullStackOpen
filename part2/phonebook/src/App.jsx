e3fb476623340e6f49ff322db221b89aa1d9e741
Moises Alfaro <moisesrolando.23@gmail.com>
2026-08-12 19:22:05 -0600

    complete exercise part2:2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        console.log(res.data)
+        setPersons(res.data)
+      })
+  }, [])
 
   const handleName = (e) => setNewName(e.target.value)
   const handleNumber = (e) => setNewNumber(e.target.value)
@@ -36,7 +41,8 @@ const App = () => {
 
     const obj = {
       name: newName,
-      number: newNumber 
+      number: newNumber,
+      id: persons.length + 1
     }
 
     setPersons(persons.concat(obj))
